1d0e23e58d0873553c50485c4cc57726d24a6a12
KUMADA Kosei <136041105+s1300218@users.noreply.github.com>
2023-06-15 18:03:38 +0900

    Greetings added

--- ex01.c
+++ ex01.c
@@ -3,7 +3,11 @@
 #include <time.h>
 int main(){
   int i,num,total=0;
+  char name[100];
   srand((unsigned int)time(NULL));
+  printf("What is your name?\n");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
   printf("Rolling dice...\n");
   for(i=1;i<3;i++){
     num=rand()%6+1;
@@ -13,3 +17,4 @@ int main(){
   printf("Total value: %d\n",total);
   return 0;
 }
+
