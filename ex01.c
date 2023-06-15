a0df7d3acb7869581ee489cf41ca601f039ba85c
KUMADA Kosei <136041105+s1300218@users.noreply.github.com>
2023-06-15 17:55:22 +0900

    Dice game: initial revision

---
+++ ex01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+  int i,num,total=0;
+  srand((unsigned int)time(NULL));
+  printf("Rolling dice...\n");
+  for(i=1;i<3;i++){
+    num=rand()%6+1;
+    printf("Die %d: %d\n",i,num);
+    total+=num;
+  }
+  printf("Total value: %d\n",total);
+  return 0;
+}
